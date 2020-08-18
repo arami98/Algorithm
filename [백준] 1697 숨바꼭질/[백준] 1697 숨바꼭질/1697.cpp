3ac936537dcfc658af26a52d751c250fbd309e1e
arami98 <arami98@naver.com>
2020-08-18 21:38:28 +0900

    2020/08/18

---
+++ [백준] 1697 숨바꼭질/[백준] 1697 숨바꼭질/1697.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#define MAX 100001
+using namespace std;
+
+int visit[MAX];
+bool inRange(int a) {
+	return (0 <= a && a <= MAX);
+}
+
+void sumba(int a, int b) {
+	queue<int> q;
+	q.push(a);
+	visit[a] = 1;
+	int time = 0;
+
+	while (!q.empty()) {
+		int qsize = q.size();
+
+		while (qsize--) {
+			int cur = q.front();
+			if (cur == b) {
+				cout << time << endl;
+				return;
+			}
+
+			int next = cur + 1;
+			if (inRange(next)) {
+				if (visit[next] == 0) {
+					q.push(next);
+					visit[next] = 1;
+				}
+			}
+
+			next = cur - 1;
+			if (inRange(next)) {
+				if (visit[next] == 0) {
+					q.push(next);
+					visit[next] = 1;
+				}
+			}
+
+			next = cur * 2;
+			if (inRange(next)) {
+				if (visit[next] == 0) {
+					q.push(next);
+					visit[next] = 1;
+				}
+			}
+
+			q.pop();
+		}
+		time++;
+	}
+}
+
+int main() {
+	int N;
+	int K;
+	cin >> N >> K;
+
+	sumba(N, K);
+	return 0;
+}
